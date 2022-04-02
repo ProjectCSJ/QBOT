85955e66d9a14c31602688bff3e57347dd41ed4a
友人Ｓ☄プロジェクトCSJ所属 <yuujins@project.csj>
2022-04-02 17:42:07 +0900

    Fix ident

--- modules/queue/queue.js
+++ modules/queue/queue.js
@@ -8,58 +8,58 @@ const sequelize = new Sequelize('database', 'username', 'password', {
 });
 
 class Queue {
-    constructor(guildId) {
-        this.guildId = guildId;
-        this.user = require('./user.js')(sequelize, Sequelize.DataTypes, guildId);
-        this.user.sync();
-        //this.user.sync({ force: true });
-    }
+	constructor(guildId) {
+		this.guildId = guildId;
+		this.user = require('./user.js')(sequelize, Sequelize.DataTypes, guildId);
+		this.user.sync();
+		// this.user.sync({ force: true });
+	}
 
-    async addUser(userId) {
-        let count = await this.getRowCount() + 1;
-        //console.log(count);
-        await this.user.create({
-            user_id: userId,
-            index: count
-        });
-    }
+	async addUser(userId) {
+		const count = await this.getRowCount() + 1;
+		// console.log(count);
+		await this.user.create({
+			user_id: userId,
+			index: count,
+		});
+	}
 
-    async swapUser(userA, userB) {
-        let a = await this.user.findOne({ where: { user_id: userA } });
-        let b = await this.user.findOne({ where: { user_id: userB } });
-        let indexA = b.index;
-        let indexB = a.index;
-        await this.user.update({ index: indexA }, { where: { user_id: a.user_id } });
-        await this.user.update({ index: indexB }, { where: { user_id: b.user_id } });
-    }
+	async swapUser(userA, userB) {
+		const a = await this.user.findOne({ where: { user_id: userA } });
+		const b = await this.user.findOne({ where: { user_id: userB } });
+		const indexA = b.index;
+		const indexB = a.index;
+		await this.user.update({ index: indexA }, { where: { user_id: a.user_id } });
+		await this.user.update({ index: indexB }, { where: { user_id: b.user_id } });
+	}
 
-    async delUser(userId) {
-        let target = await this.user.findOne({ where: { user_id: userId } });
-        let lowerUserList = await this.user.findAll({ where: { index: { [Op.gt]: target.index } }, raw: true })
-        await this.user.destroy({ where: { user_id: userId } });
-        for(let u of lowerUserList) {
-            await this.user.update({ index: u.index-1 }, { where: { user_id: u.user_id } });
-        }
-    }
+	async delUser(userId) {
+		const target = await this.user.findOne({ where: { user_id: userId } });
+		const lowerUserList = await this.user.findAll({ where: { index: { [Op.gt]: target.index } }, raw: true });
+		await this.user.destroy({ where: { user_id: userId } });
+		for (const u of lowerUserList) {
+			await this.user.update({ index: u.index - 1 }, { where: { user_id: u.user_id } });
+		}
+	}
 
-    async getUserQueue() {
-        const list = await this.user.findAll({
-            attributes: ['user_id'],
-            order:[['index', 'ASC']],
-            raw: true
-        });
-        return list;
-    }
+	async getUserQueue() {
+		const list = await this.user.findAll({
+			attributes: ['user_id'],
+			order:[['index', 'ASC']],
+			raw: true,
+		});
+		return list;
+	}
 
-    async getRowCount() {
-        let count = await this.user.count();
-        return count;
-    }
+	async getRowCount() {
+		const count = await this.user.count();
+		return count;
+	}
 
-    async getFirst() {
-        let result = await this.user.findOne({ where: { index: 1 }, raw: true });
-        return result
-    }
+	async getFirst() {
+		const result = await this.user.findOne({ where: { index: 1 }, raw: true });
+		return result;
+	}
 }
 
 module.exports.Queue = Queue;
