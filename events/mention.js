0f9d081c4da15a454e40fc09cc4019904a4f0f16
友人Ｓ <yuujins@project.csj>
2022-04-06 00:59:30 +0900

    Add mention logger

--- events/mention.js
+++ events/mention.js
@@ -1,3 +1,5 @@
+const logger = require('node-color-log');
+
 module.exports = {
 	name: 'messageCreate',
 	once: false,
@@ -5,6 +7,7 @@ module.exports = {
 		if (/<@!?948362856624189460>/.test(message.content)) {
 			// if (rows.length < 1) /* Set current to Wait for queue and set queue list to Null */;
 			// if (rows.length = 1) /* Set queue list to last one */;
+			logger.debug(`${message.author} triggered event mention`);
 			await message.author.send({ content: 'I\'ll put u on the queue' });
 		}
 	},
