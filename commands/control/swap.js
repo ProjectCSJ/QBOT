eb0f848c3ffa123fb8a04196f9fa403b86a7cb9d
友人Ｓ <yuujins@project.csj>
2022-04-06 00:59:59 +0900

    Add swap command

---
+++ commands/control/swap.js
@@ -0,0 +1,52 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.addUserOption((option) =>
+			option.setName('target')
+				.setDescription('Who U wanna swap')
+				.setRequired(true),
+		)
+		.setDefaultPermission(true)
+		.setDescription('Swap with someone in queue.')
+		.setName('swap'),
+	async execute(interaction) {
+		const user = interaction.options.getUser('target');
+		const SwapEmbed = new MessageEmbed()
+			.setAuthor({
+				iconURL: interaction.user.avatarURL({ dynamic: true }),
+				name: interaction.user.tag,
+				url: `https://discord.com/users/${interaction.user.id}`,
+			})
+			.setColor('#00D1BD')
+			.setDescription(`<@${interaction.user.id}> wanna swap with U`)
+			.setFooter({
+				iconURL: process.env.IconURL,
+				text: process.env.COPYRIGHT,
+			})
+			.setTitle('Swap');
+		const SwapAction = new MessageActionRow()
+			.addComponents(
+				new MessageButton({
+					custom_id: 'yes',
+					label: 'Yes',
+					style: 'SUCCESS',
+				}),
+			)
+			.addComponents(
+				new MessageButton({
+					custom_id: 'no',
+					label: 'No',
+					style: 'DANGER',
+				}),
+			);
+		await user.send({
+			components: [SwapAction],
+			embeds: [SwapEmbed],
+		});
+		await interaction.reply({
+			content: `I already ask <@${user.id}>`,
+		});
+	},
+};
