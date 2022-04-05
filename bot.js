186dcb0528442cedbfe2c99694c72d5c79819313
友人Ｓ <yuujins@project.csj>
2022-04-05 18:51:51 +0900

    Update eslint rules

--- bot.js
+++ bot.js
@@ -1,6 +1,3 @@
-/* eslint-disable max-len */
-/* eslint-disable no-tabs */
-
 // Console Settings
 const logger = require('node-color-log');
 logger.setDate(() => (new Date()).toLocaleString());
@@ -13,7 +10,14 @@ const fs = require('fs');
 // Modules Import
 const { Client, Collection, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES] });
+const client = new Client({
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.DIRECT_MESSAGES,
+	],
+});
 
 client.commands = new Collection();
 
@@ -50,7 +54,10 @@ client.on('interactionCreate', async (interaction) => {
 	}
 	catch (error) {
 		logger.warn(`${error}`);
-		await interaction.reply({ content: `Command Error!\nSend the error message screenshot to <@826327097945489408>\nError:\n\`\`\`${error}\`\`\``, ephemeral: true });
+		await interaction.reply({
+			content: `Command Error!\nSend the error message screenshot to <@826327097945489408>\nError:\n\`\`\`${error}\`\`\``,
+			ephemeral: true,
+		});
 	}
 });
 
