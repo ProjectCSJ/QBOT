096c9c155a36992e5393fd250a4e9742f0f4457f
友人Ｓ <yuujins@project.csj>
2022-04-07 14:47:15 +0900

    Fix repeat queue

--- events/mention.js
+++ events/mention.js
@@ -8,11 +8,17 @@ module.exports = {
 	async execute(message) {
 		if (/<@!?948362856624189460>/.test(message.content)) {
 			const queue = new Queue(message.guild.id);
-			await queue.addUser(message.author.id);
 			// if (rows.length < 1) /* Set current to Wait for queue and set queue list to Null */;
 			// if (rows.length = 1) /* Set queue list to last one */;
 			logger.debug(`${message.author.tag} triggered event mention`);
-			await message.author.send({
+			const result = await queue.addUser(message.author.id);
+			if (result === 'error') {
+				return await message.author.send({
+					content: 'U can\'t join the queue when U still in',
+					ephemeral: false,
+				});
+			}
+			return await message.author.send({
 				content: 'I\'ll put u on the queue',
 				ephemeral: false,
 			});
