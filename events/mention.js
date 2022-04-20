5bb18c76f6daaba8dc4eeb877e8e5e6b52c92244
友人Ｓ <yuujins@project.csj>
2022-04-20 09:01:03 +0900

    Fix bugs
    Co-authored-by: Pierre <pierre0210@users.noreply.github.com>

--- events/mention.js
+++ events/mention.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 const logger = require('node-color-log');
 const { Queue } = require('../modules/queue/queue');
 
@@ -7,11 +7,97 @@ module.exports = {
 	once: false,
 	async execute(message) {
 		if (/<@!?948362856624189460>/.test(message.content)) {
-			const queue = new Queue(message.guild.id);
-			// if (rows.length < 1) /* Set current to Wait for queue and set queue list to Null */;
-			// if (rows.length = 1) /* Set queue list to last one */;
+			const QueueAction = new MessageActionRow()
+				.addComponents(
+					new MessageButton({
+						custom_id: 'next',
+						label: 'Next one',
+						style: 'SUCCESS',
+					}),
+				)
+				.addComponents(
+					new MessageButton({
+						custom_id: 'remove',
+						label: 'Remove From Queue',
+						style: 'DANGER',
+					}),
+				);
+
+			await message.delete();
+			if (message.guild.me.voice.channel === null) return await message.author.send('No queue started.');
+			const queue = new Queue(message.guildId);
 			logger.debug(`${message.author.tag} triggered event mention`);
 			const result = await queue.addUser(message.author.id);
+			const QueueStatus = new MessageEmbed();
+			const QueueRowCount = await queue.getRowCount();
+			if (QueueRowCount === 1) {
+				const now = await queue.getFirst();
+				QueueStatus
+					.addFields(
+						{
+							name: 'Now Singing',
+							value: `<@${now.user_id}>`,
+						},
+						{
+							name: 'Queue list',
+							value: '**Last One!**',
+						},
+					)
+					.setAuthor({
+						name: process.env.AuthorName,
+						iconURL: process.env.IconURL,
+						url: process.env.SiteURL,
+					})
+					.setColor('#00D1BD')
+					.setDescription(`Here's queue in ${message.guild.name}!\nUsing button to control`)
+					.setFooter({
+						text: process.env.COPYRIGHT,
+						iconURL: process.env.IconURL,
+					})
+					.setTitle('Queue');
+			}
+			if (QueueRowCount > 1) {
+				const now = await queue.getFirst();
+				let list = '';
+				const QueueList = await queue.getUserQueue();
+				QueueList.forEach((element) => {
+					logger.debug(element);
+					if (element.index !== 1) {
+						list += '<@' + element.user_id + '>\n';
+					}
+				});
+				QueueStatus
+					.addFields(
+						{
+							name: 'Now Singing',
+							value: `<@${now.user_id}>`,
+						},
+						{
+							name: 'Queue list',
+							value: list,
+						},
+					)
+					.setAuthor({
+						name: process.env.AuthorName,
+						iconURL: process.env.IconURL,
+						url: process.env.SiteURL,
+					})
+					.setColor('#00D1BD')
+					.setDescription(`Here's queue in ${message.guild.name}!\nUsing button to control`)
+					.setFooter({
+						text: process.env.COPYRIGHT,
+						iconURL: process.env.IconURL,
+					})
+					.setTitle('Queue');
+			}
+			const threadname = process.env.QueueName;
+			const thread = message.channel.threads.cache.find((x) => x.name.startsWith(threadname));
+			const QueueMessage = thread.messages.cache.find((x) => x.content === 'Queue Start!');
+			await QueueMessage.edit({
+				components: [QueueAction],
+				content: 'Queue Start!',
+				embeds: [QueueStatus],
+			});
 			if (result === 'error') {
 				return await message.author.send({
 					content: 'U can\'t join the queue when U still in',
