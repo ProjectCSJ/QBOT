222d5016c10419c46dc01317a5b3cb63f82ab255
音羽みのり <OtohaMinori@users.noreply.github.com>
2022-06-09 11:58:04 +0900

    Update codes

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -7,19 +7,18 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		console.log(interaction.guild.me.avatarURL())
 		const ping = new MessageEmbed()
 			.setColor('RANDOM')
 			.setAuthor({
 				name: interaction.guild.me.displayName,
-				iconURL: `${interaction.guild.me.avatarURL()}`,
+				iconURL: interaction.guild.me.avatarURL(),
 				url: process.env.SiteURL,
 			})
 			.setTitle('Pong!')
 			.setDescription(`延遲${Math.abs(Date.now() - interaction.createdTimestamp)}ms.`)
 			.setFooter({
 				text: process.env.COPYRIGHT,
-				iconURL: `${interaction.guild.me.avatarURL()}`,
+				iconURL: interaction.guild.me.avatarURL(),
 			});
 		await interaction.reply({
 			embeds: [ping],
